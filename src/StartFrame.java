fd99c182afa97333eac27be062c931fcdd2c74ab
MinseoKangQ <kms02171@naver.com>
2022-11-23 13:51:43 +0900

    StartFrame 수정(ToolBar 부착)

--- src/StartFrame.java
+++ src/StartFrame.java
@@ -10,6 +10,12 @@ public class StartFrame extends JFrame{
 	
 	private PlayerSettingDialog playersettingdialog; // 다이얼로그 레퍼런스
 	
+//	private ImageIcon normalIcon = new ImageIcon("PlayButton.png");
+//	private ImageIcon overedIcon = new ImageIcon("PlayButton.png");
+//	private ImageIcon pressedIcon = new ImageIcon("PlayButton.png");
+//	private JButton startBtn = new JButton(normalIcon);
+	
+	
 	private Clip clip;
 	
 	// StartFrame 생성자
@@ -17,20 +23,33 @@ public class StartFrame extends JFrame{
 		
 		setTitle("Typing Game"); // Title 설정
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 창 닫히면 프로그램 종료
-		Container c = getContentPane();
 		
-		c.setLayout(new BorderLayout());
+		Container container = getContentPane(); // 컨텐트팬 받아오기
+		container.setLayout(new BorderLayout()); // 컨테이너 배치관리자 설정
 		
-		c.add(new StartFrameNorthPanel(), BorderLayout.NORTH);
-		c.add(new StartFrameCenterPanel(), BorderLayout.CENTER);
-		c.add(new StartFrameSouthPanel(), BorderLayout.SOUTH);
+		// 컨테이너에 부착할 패널들 생성
+		StartFrameNorthPanel startFrameNorthPanel = new StartFrameNorthPanel();
+		StartFrameCenterPanel startFrameCenterPanel = new StartFrameCenterPanel();
+		StartFrameSouthPanel startFrameSouthPanel = new StartFrameSouthPanel();
+		
+		// 패널들 컨테이너에 부착
+		container.add(startFrameNorthPanel, BorderLayout.NORTH);
+		container.add(startFrameCenterPanel, BorderLayout.CENTER);
+		container.add(startFrameSouthPanel, BorderLayout.SOUTH);
+		
+		// startFrameCenterPanel에 부착할 패널들 생성 
+		StartFrameCenterNorthPanel startFrameCenterNorthPanel = new StartFrameCenterNorthPanel();
+		StartFrameCenterCenterPanel startFrameCenterCenterPanel = new StartFrameCenterCenterPanel();
+		
+		// 패널들 startFrameCenterPanel에 부착
+		startFrameCenterPanel.add(startFrameCenterNorthPanel, BorderLayout.NORTH);
+		startFrameCenterPanel.add(startFrameCenterCenterPanel, BorderLayout.CENTER);
 		
 		setSize(900, 680); // 창 크기
 		setVisible(true); // 창 보이게
 		setResizable(false); // 창 크기 변경 불가능하게	
 		
-	
-					
+		
 		// **오디오 => Thread로 만들자
 		try {
 			clip = AudioSystem.getClip();
@@ -49,9 +68,55 @@ public class StartFrame extends JFrame{
 
 	}
 	
+	// 새로 만들었음
 	class StartFrameNorthPanel extends JPanel {
 		
 		public StartFrameNorthPanel() {
+			
+			// 툴바 만들기
+			JToolBar toolBar = new JToolBar();
+			
+			toolBar.add(new JButton("NEW"));
+			toolBar.addSeparator();
+			this.add(toolBar, new FlowLayout());
+			
+			// ** 툴바 레이아웃 null?
+			// ** PlayButton, StopButton, ResetButton 붙이기
+			// **
+		
+			toolBar.setFloatable(false); // 툴바 이동 못하게 설정
+			// ** 시작하면 포커스 ??
+			//toolBar.setFocusable(false); -> ?
+//			
+			// 액션 리스너 등록 
+//			startBtn.addActionListener(new ActionListener() {
+//				
+//				public void actionPerformed(ActionEvent e) {
+//					
+//					// 소리 세팅
+//				}
+//			});
+//			
+			// 툴바에 버튼 달기
+//			tBar.add(startBtn);
+			
+			
+		}
+		
+	}
+	
+	class StartFrameCenterPanel extends JPanel {
+		
+		public StartFrameCenterPanel() {
+			
+			setLayout(new BorderLayout());
+			
+		}
+	}
+	// 수정됨
+	class StartFrameCenterNorthPanel extends JPanel {
+		
+		public StartFrameCenterNorthPanel() {
 			
 			setBackground(Color.YELLOW);
 			setLayout(new FlowLayout());
@@ -65,9 +130,10 @@ public class StartFrame extends JFrame{
 		
 	}
 	
-	class StartFrameCenterPanel extends JPanel {
+	// 수정됨
+	class StartFrameCenterCenterPanel extends JPanel {
 		
-		public StartFrameCenterPanel() {
+		public StartFrameCenterCenterPanel() {
 			
 			setBackground(Color.YELLOW);
 			setLayout(new FlowLayout());
@@ -120,6 +186,8 @@ public class StartFrame extends JFrame{
 			
 		}
 	}
-	
+
+			
 }
+		
 // StartFrame 끝
